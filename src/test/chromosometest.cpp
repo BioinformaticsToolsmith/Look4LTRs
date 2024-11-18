09724fd26dc9b1757d022688ed973779f83bb9c9
Jason Stajich <jason.stajich@ucr.edu>
2024-11-17 21:35:33 -0800

    comment hard coded user path
    Bad code engineering practice to have hard coded path to user DIR.
    Also the signature of this obj doesn't match expected so this won't compile right now anyways.

--- src/test/chromosometest.cpp
+++ src/test/chromosometest.cpp
@@ -6,7 +6,7 @@
 int main() {
 
     //ChromosomeOneDigit * c = new ChromosomeOneDigit("/home/transposons/Genomes/TAIR10/Fasta/TAIR10_chr1.fa");
-    TableBuilder *tb = new TableBuilder("/home/transposons/Genomes/TAIR10/Fasta/");
+    //TableBuilder *tb = new TableBuilder("/home/transposons/Genomes/TAIR10/Fasta/");
 
     // delete c;
 
